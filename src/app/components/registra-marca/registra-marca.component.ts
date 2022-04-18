c2a53eed23b6ca5130793e9ab7609ba5163b916a
cevm_cesar <cevm_cesar@hotmail.com>
2022-04-18 17:05:38 -0500

    Avance de Front End - Marca

--- src/app/components/registra-marca/registra-marca.component.ts
+++ src/app/components/registra-marca/registra-marca.component.ts
@@ -1,3 +1,7 @@
+import { Marca } from 'src/app/models/marca.model';
+import { MarcaService } from 'src/app/services/marca.service';
+import { Pais } from 'src/app/models/pais.model';
+import { PaisService } from 'src/app/services/pais.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistraMarcaComponent implements OnInit {
 
-  constructor() { }
+  paises: Pais[] = [];
+  marca: Marca = {
+    pais:{
+      idPais:-1
+
+    }
+  }
+
+  constructor(private paisService: PaisService, private marcaService: MarcaService) {
+
+    this.paisService.listaPais().subscribe(
+      (x) => this.paises = x
+    );
+  }
+
+  insertaMarca() {
+    this.marcaService.insertaMarca(this.marca).subscribe(
+      response => {
+        alert(response.mensaje);
+      },
+      error => {
+        alert(error);
+        console.log(error);
+      }
+
+    );
+  }
 
   ngOnInit(): void {
   }
