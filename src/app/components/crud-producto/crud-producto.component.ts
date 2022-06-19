477d6590220553fea63cd83d21ea95e0a2463807
jossrc <kuro.rc10@gmail.com>
2022-06-18 21:59:29 -0500

    inicio crud-producto

--- src/app/components/crud-producto/crud-producto.component.ts
+++ src/app/components/crud-producto/crud-producto.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Marca } from 'src/app/models/marca.model';
+import { Producto } from 'src/app/models/producto.model';
+import { MarcaService } from 'src/app/services/marca.service';
+import { PaisService } from 'src/app/services/pais.service';
+import { ProductoService } from 'src/app/services/producto.service';
+import { Pais } from '../../models/pais.model';
 
 @Component({
   selector: 'app-crud-producto',
@@ -7,9 +13,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudProductoComponent implements OnInit {
 
-  constructor() { }
+  listaPaises: Pais[] = [];
+  listaMarcas: Marca[] = [];
+  listaProductos: Producto[] = [];
+  mensajeDeConsulta: string = '';
 
-  ngOnInit(): void {
+  filtroNombre: string = '';
+
+  objProducto: Producto = {
+    estado : 1,
+    pais: {
+      idPais: -1,
+    },
+    marca: {
+      idMarca: -1,
+    },
+  };
+
+  constructor(
+    private paisService: PaisService,
+    private marcaService: MarcaService,
+    private productoService: ProductoService
+  ) {
+    this.llenarCombos();
+    this.consultarProductos();
   }
 
+  ngOnInit(): void {}
+
+  private llenarCombos() {
+    this.cargarPaises();
+    this.cargarMarcas();
+  }
+
+  private cargarPaises() {
+    this.paisService.listaPais().subscribe((paises: Pais[]) => {
+      if (paises && paises.length > 0) {
+        this.listaPaises = paises;
+      }
+    });
+  }
+
+  private cargarMarcas() {
+    this.marcaService.listaMarca().subscribe((marcas: Marca[]) => {
+      if (marcas && marcas.length > 0) {
+        this.listaMarcas = marcas;
+      }
+    });
+  }
+
+  public consultarProductos() {
+    let { nombre, durabilidad, marca, pais, estado } = this.objProducto;
+
+    // Si estado es true o 1
+    if (estado) {
+      estado = 1
+    } else {
+      estado = 0
+    }
+
+    this.productoService
+      .consultar({nombre,estado})
+      .subscribe(
+        (res: any) => {
+          this.mensajeDeConsulta = res.mensaje;
+          if (res && res.data && res.data.length > 0) {
+            this.listaProductos = res.data;
+          }else {
+            this.listaProductos = [];
+          }
+        },
+        (err) => {
+          console.log('HAY UN ERROR :: ', err);
+          alert('Sucedió un error inesperado consulte con su administrador');
+        }
+      );
+  }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -17,14 +17,22 @@ export class ProductoService {
     return this.http.post<any>(baseUrlProducto, data);
   }
 
-  public consultar(
-    nombre: string = '',
-    durabilidad: string = '',
-    marcaId: number = -1,
-    paisId: number = -1,
-    estado: number = -1,
-  ): Observable<any> {
+  public consultar(data: {
+    nombre?: string;
+    durabilidad?: string;
+    marcaId?: number;
+    paisId?: number;
+    estado?: number;
+  }): Observable<any> {
     let url = baseUrlProducto + '/listarPorFiltros';
+    const {
+      nombre = '',
+      durabilidad = '',
+      marcaId = -1,
+      paisId = -1,
+      estado = -1,
+    } = data;
+
     const params = new HttpParams()
       .append('nombre', nombre)
       .append('durabilidad', durabilidad)
