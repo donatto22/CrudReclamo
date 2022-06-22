622001c65644ed98761f661e3d127ffa5bb439d9
eustakio1661 <diegoagutarrac@gmail.com>
2022-06-22 02:31:03 -0500

    Probando registrar proveedor

--- src/app/components/crud-proveedor/crud-proveedor.component.ts
+++ src/app/components/crud-proveedor/crud-proveedor.component.ts
@@ -24,6 +24,13 @@ export class CrudProveedorComponent implements OnInit {
   filtroEstado: number = 1;
 
   objProveedor: Proveedor = {
+    idProveedor: 0,
+    razonsocial: "",
+    ruc: "",
+    direccion: "",
+    telefono: "",
+    celular: "",
+    contacto: "",
     estado: 1,
     ubigeo: {
       idUbigeo: -1,
@@ -32,7 +39,7 @@ export class CrudProveedorComponent implements OnInit {
       distrito: "",
     }
   };
-  
+
   idProveedor: number = -1;
 
 
@@ -40,7 +47,7 @@ export class CrudProveedorComponent implements OnInit {
     razonsocial: new FormControl('', [Validators.required, Validators.maxLength(45)]),
     ruc: new FormControl('', [Validators.required, Validators.maxLength(11), Validators.pattern('^[0-9]*$')]),
     direccion: new FormControl('', [Validators.required, Validators.maxLength(45)]),
-    telefono: new FormControl('', [Validators.required,Validators.maxLength(9), Validators.pattern('^[0-9]*$')]),
+    telefono: new FormControl('', [Validators.required, Validators.maxLength(9), Validators.pattern('^[0-9]*$')]),
     celular: new FormControl('', [Validators.required, Validators.maxLength(9), Validators.pattern('^[0-9]*$')]),
     contacto: new FormControl('', [Validators.required, Validators.maxLength(45)]),
     departamentoId: new FormControl(-1, [Validators.required, Validators.min(1)]),
@@ -52,7 +59,7 @@ export class CrudProveedorComponent implements OnInit {
     razonsocial: new FormControl('', [Validators.required, Validators.maxLength(45)]),
     ruc: new FormControl('', [Validators.required, Validators.maxLength(11), Validators.pattern('^[0-9]*$')]),
     direccion: new FormControl('', [Validators.required, Validators.maxLength(45)]),
-    telefono: new FormControl('', [Validators.required,Validators.maxLength(9), Validators.pattern('^[0-9]*$')]),
+    telefono: new FormControl('', [Validators.required, Validators.maxLength(9), Validators.pattern('^[0-9]*$')]),
     celular: new FormControl('', [Validators.required, Validators.maxLength(9), Validators.pattern('^[0-9]*$')]),
     contacto: new FormControl('', [Validators.required, Validators.maxLength(45)]),
     departamentoId: new FormControl(-1, [Validators.required, Validators.min(1)]),
@@ -60,58 +67,99 @@ export class CrudProveedorComponent implements OnInit {
     distritoId: new FormControl(-1, [Validators.required, Validators.min(1)]),
   });
 
+  submitted = false;
+
   constructor(private ubigeoService: UbigeoService,
-    private proveedorService: ProveedorService) { 
-      this.ubigeoService.listarDepartamento().subscribe(
-        response => this.departamentos = response
-      );
-      this.consultarProveedores();
+    private proveedorService: ProveedorService) {
+    this.ubigeoService.listarDepartamento().subscribe(
+      response => this.departamentos = response
+    );
+    this.consultarProveedores();
+  }
+
+  cargaProvincia() {
+    this.ubigeoService.listaProvincias(this.objProveedor.ubigeo?.departamento).subscribe(
+      response => this.provincias = response
+    );
+
+    this.objProveedor!.ubigeo!.provincia = "-1";
+    this.distritos = [];
+    this.objProveedor!.ubigeo!.idUbigeo = -1;
+
+  }
+
+  cargaDistrito() {
+    this.ubigeoService.listaDistritos(this.objProveedor.ubigeo?.departamento, this.objProveedor.ubigeo?.provincia).subscribe(
+      response => this.distritos = response
+    );
+
+    this.objProveedor!.ubigeo!.idUbigeo = -1;
+  }
+
+  consultarProveedores() {
+    if (this.filtroEstado) {
+      this.filtroEstado = 1;
+    } else {
+      this.filtroEstado = 0;
     }
 
-    cargaProvincia() {
-      this.ubigeoService.listaProvincias(this.objProveedor.ubigeo?.departamento).subscribe(
-        response => this.provincias = response
-      );
-  
-      this.objProveedor!.ubigeo!.provincia = "-1";
-      this.distritos = [];
-      this.objProveedor!.ubigeo!.idUbigeo = -1;
-  
-    }
-  
-    cargaDistrito() {
-      this.ubigeoService.listaDistritos(this.objProveedor.ubigeo?.departamento, this.objProveedor.ubigeo?.provincia).subscribe(
-        response => this.distritos = response
-      );
-  
-      this.objProveedor!.ubigeo!.idUbigeo = -1;
-    }
-
-    consultarProveedores() {
-      if (this.filtroEstado) {
-        this.filtroEstado = 1;
-      } else {
-        this.filtroEstado = 0;
-      }
-  
-      this.proveedorService
-        .consultarProveedor({ruc: this.filtroRazonRuc,estado: this.filtroEstado })
-        .subscribe(
-          (res: any) => {
-            this.mensajeDeConsulta = res.mensaje;
-            if (res && res.data && res.data.length > 0) {
-              this.listaProveedor = res.data;
-            } else {
-              this.listaProveedor = [];
-            }
-          },
-          (err) => {
-            console.log('HAY UN ERROR :: ', err);
+    this.proveedorService
+      .consultarProveedor({ ruc: this.filtroRazonRuc, estado: this.filtroEstado })
+      .subscribe(
+        (res: any) => {
+          this.mensajeDeConsulta = res.mensaje;
+          if (res && res.data && res.data.length > 0) {
+            this.listaProveedor = res.data;
+          } else {
+            this.listaProveedor = [];
           }
-        );
-    }
+        },
+        (err) => {
+          console.log('HAY UN ERROR :: ', err);
+        }
+      );
+  }
 
   ngOnInit(): void {
+  }
+
+  registraProveedor() {
+    this.submitted = true;
+
+    if (this.formRegistrarProveedor.invalid) {
+      return;
+    }
+
+    this.submitted = false;
+
+    this.proveedorService.insertarProveedor(this.objProveedor).subscribe(
+      (x) => {
+        this.submitted = false;
+        document.getElementById("btn_reg_cerrar")?.click();
+        Swal.fire('Mensaje', x.mensaje, 'success');
+        this.consultarProveedores();
+      }
+    );
+
+    this.distritos = [];
+    this.provincias = [];
+
+    this.objProveedor = {
+      idProveedor: 0,
+      razonsocial: "",
+      ruc: "",
+      direccion: "",
+      telefono: "",
+      celular: "",
+      contacto: "",
+      estado: 1,
+      ubigeo: {
+        idUbigeo: -1,
+        departamento: "-1",
+        provincia: "-1",
+        distrito: "",
+      }
+    }
   }
 
 }
